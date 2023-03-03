15fe81ac7113baaac460549b88eb81c26135aa4f
Ash <plase024@morris.umn.edu>
2023-03-03 12:29:42 -0600

    Add injectors for missing test components

--- client/src/app/items/item-list.component.spec.ts
+++ client/src/app/items/item-list.component.spec.ts
@@ -1,21 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockItemService } from 'src/testing/item.service.mock';
+import { ItemCardComponent } from './item-card.component';
 
 import { ItemListComponent } from './item-list.component';
+import { ItemService } from './item.service';
+
+const COMMON_IMPORTS: unknown[] = [
+  FormsModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatInputModule,
+  MatExpansionModule,
+  MatTooltipModule,
+  MatListModule,
+  MatDividerModule,
+  MatRadioModule,
+  MatIconModule,
+  MatSnackBarModule,
+  BrowserAnimationsModule,
+  RouterTestingModule,
+];
 
 describe('ItemListComponent', () => {
   let component: ItemListComponent;
   let fixture: ComponentFixture<ItemListComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ItemListComponent ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ItemListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [COMMON_IMPORTS],
+      declarations: [ItemListComponent, ItemCardComponent],
+      providers: [{ provide: ItemService, useValue: new MockItemService() }]
+    });
   });
+
+  beforeEach(waitForAsync(() => {
+  // Compile all the components in the test bed
+  // so that everything's ready to go.
+    TestBed.compileComponents().then(() => {
+      fixture = TestBed.createComponent(ItemListComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
---
+++ client/src/testing/item.service.mock.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@angular/core";
+import { of } from "rxjs";
+import { Observable } from "rxjs/internal/Observable";
+import { AppComponent } from "src/app/app.component";
+import { Item, FoodCategory } from "src/app/items/item";
+import { ItemService } from "src/app/items/item.service";
+
+/**
+ * A "mock" version of the `UserService` that can be used to test components
+ * without having to create an actual service. It needs to be `Injectable` since
+ * that's how services are typically provided to components.
+ */
+@Injectable({
+  providedIn: AppComponent
+})
+export class MockItemService extends ItemService {
+  static testItems: Item[] = [
+    {
+      _id: "588935f5556f992bf8f37c01",
+      name: "Apples",
+      category: 'fruits',
+      unit: "item",
+      count: 32,
+      price: 10.23,
+      priority: 2,
+      date_added: "2023-02-28T19:17:04Z",
+      date_updated: "2023-02-28T19:17:04Z",
+      count_remaining: 2
+    },
+    {
+      _id: "588935f5556f992bf8f37c02",
+      name: "Jon",
+      category: 'household',
+      unit: "cups",
+      count: 100,
+      price: 9.12,
+      priority: 8,
+      date_added: "2023-01-11T19:17:04Z",
+      date_updated: "2023-01-11T19:17:04Z",
+      count_remaining: 20}
+  ];
+
+  constructor() {
+    super(null);
+  }
+
+  getItems(filters?: { category?: FoodCategory; count?: number; unit?: string }): Observable<Item[]> {
+    return of(MockItemService.testItems);
+  }
+}

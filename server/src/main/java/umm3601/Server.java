4d1453d218431ad152e2c1b442beb73f4f41e26e
ChenfeiP <peng0368@morris.umn.edu>
2023-03-04 14:53:40 -0600

    Change back to port 4567

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -18,7 +18,7 @@ import umm3601.user.UserController;
 
 public class Server {
 
-  private static final int SERVER_PORT = 8000;
+  private static final int SERVER_PORT = 4567;
 
   public static void main(String[] args) {
 

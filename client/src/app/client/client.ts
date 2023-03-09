d26402f355e5d285bbfb0565d1c30a465d0b8a1b
Harry <lv000013@morris.umn.edu>
2023-03-09 14:34:00 -0600

    Add client service

---
+++ client/src/app/client/client.ts
@@ -0,0 +1,8 @@
+/* eslint @typescript-eslint/naming-convention: "off" */
+export interface ClientRequest {
+    _id: string;
+    need_met: boolean;
+    item_description: string;
+    date_added: string;
+
+  }

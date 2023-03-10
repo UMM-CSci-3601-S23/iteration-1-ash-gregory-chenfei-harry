49b1f2baedea9fc3e73fc676740fd74074117124
Gregory Peterson <pet03356@morris.umn.edu>
2023-03-09 18:01:27 -0600

    Add Display of client requests

--- client/src/app/client/client.component.spec.ts
+++ client/src/app/client/client.component.spec.ts
@@ -1,20 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockClientService } from 'src/testing/client.service.mock';
 
 import { ClientComponent } from './client.component';
+import { ClientService } from './client.service';
 
 describe('ClientComponent', () => {
   let component: ClientComponent;
   let fixture: ComponentFixture<ClientComponent>;
+  let form: FormGroup;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ClientComponent ]
-    })
-    .compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.overrideProvider(ClientService, { useValue: new MockClientService() });
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        RouterTestingModule
+      ],
+      declarations: [ClientComponent],
+    }).compileComponents().catch(error => {
+      expect(error).toBeNull();
+    });
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ClientComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    form = component.addClientRequestForm;
+    expect(form).toBeDefined();
+    expect(form.controls).toBeDefined();
   });
 
   it('should create', () => {
